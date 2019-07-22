7731c5a6ead5f97f33f39a0f0d5128b713581138
MonkeyDkon <kon_fu2@hotmail.com>
2019-07-22 15:44:10 +0300

    signup with email pass works

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,39 @@
 const { validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
 exports.login = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-
-    res.status(200).json({email: email, password: password});
+    let loadedUser;
+    
 };
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
-    console.log(errors);
-    res.status(200).json({test: "works"});
+    if(!errors.isEmpty()){
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        errors.data = errors.array();
+        throw error;
+    }
+    const email = req.body.email;
+    const password = req.body.password;
+    bcrypt.hash(password, 12)
+        .then(hashedPassword => {
+            const user = new User({
+                email: email,
+                password: hashedPassword
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.status(201).json({ message: 'user created', userId: result._id});
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
