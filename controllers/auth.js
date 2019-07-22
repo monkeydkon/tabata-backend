1405392bb42f2879b39b580dcca59fd67bc3db38
MonkeyDkon <kon_fu2@hotmail.com>
2019-07-22 20:12:41 +0300

    email login done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,48 @@
 const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.login = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     let loadedUser;
-    
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('There is no user with this email');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password)
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong password');
+                error.statusCode = 401;
+                throw error;
+            }
+            // if password is correct generate a JWT
+            const token = jwt.sign({
+                email: loadedUser.email, userId: loadedUser._id.toString()
+            }, 'secret',
+                {
+                    expiresIn: '1h'
+                });
+            res.status(200).json({ token: token, userId: loadedUser._id.toString() });     //send back token
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
 };
 
 exports.signup = (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         const error = new Error('Validation failed');
         error.statusCode = 422;
         errors.data = errors.array();
@@ -28,10 +59,10 @@ exports.signup = (req, res, next) => {
             return user.save();
         })
         .then(result => {
-            res.status(201).json({ message: 'user created', userId: result._id});
+            res.status(201).json({ message: 'user created', userId: result._id });
         })
         .catch(err => {
-            if(!err.statusCode){
+            if (!err.statusCode) {
                 err.statusCode = 500;
             }
             next(err);
