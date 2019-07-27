6a9fda2510a0a247fff79c0d496d04ea3fd38a21
MonkeyDkon <kon_fu2@hotmail.com>
2019-07-27 16:56:10 +0300

    delete & update tabata done

--- controllers/actions.js
+++ controllers/actions.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-
+const { validationResult } = require('express-validator/check');
 
 exports.getTabata = (req, res, next) => {
     const userId = req.userId;
@@ -11,7 +11,7 @@ exports.getTabata = (req, res, next) => {
                 error.statusCode = 500;
                 throw error;
             }
-            return user
+            return user;
         })
         .then(result => {
              const tabata = result.tabatas.find(tabata => tabata._id == tabataId);
@@ -50,6 +50,13 @@ exports.getTabatas = (req, res, next) => {
 };
 
 exports.postTabata = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        errors.data = errors.array();
+        throw error;
+    }
     const userId = req.userId;
     const name = req.body.name;
     const work = req.body.work;
@@ -77,7 +84,7 @@ exports.postTabata = (req, res, next) => {
             user.tabatas.push(tabata);
             return user.save();
         }).then(result => {
-            res.status(200).json(result);
+            res.status(200).json({message: 'new tabata added'});
         })
         .catch(err => {
             if (!err.statusCode) {
@@ -85,6 +92,85 @@ exports.postTabata = (req, res, next) => {
             }
             next(err);
         });
+};
 
+exports.deleteTabata = (req, res, next) => {
+    const userId = req.userId;
+    const tabataId = req.params.tabataId;
+    User.findById(userId)
+        .then(user => {
+            if(!user){
+                const error = new Error('This user doesnt exist');
+                error.statusCode = 404;
+                throw error; 
+            }
+            return user;
+        })
+        .then(user => {
+            const tabata = user.tabatas.findIndex(tabata => tabata._id == tabataId);
+             if(tabata == -1){
+                const error = new Error('Tabata doesnt exist');
+                error.statusCode = 500;
+                throw error;
+             }
+             const updatedTabatas = user.tabatas.splice(tabata, 1);  
+             user.updateOne({
+                 tabatas: updatedTabatas
+             })
+             return user.save();
+        })
+        .then(result => {
+            res.status(200).json({message: 'tabata deleted successfully'});
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
+
+exports.updateTabata = (req, res, next) => {
+    const userId = req.userId;
+    const tabataId = req.params.tabataId;
+    const name = req.body.name;
+    const work = req.body.work;
+    const rest = req.body.rest;
+    const rounds = req.body.rounds;
+    const prepare = req.body.prepare;
+    const description = req.body.description;
+    User.findById(userId)
+        .then(user => {
+            if(!user){
+                const error = new Error('This user doesnt exist');
+                error.statusCode = 404;
+                throw error; 
+            }
+            return user;
+        })
+        .then(user => {
+            const tabata = user.tabatas.find(tabata => tabata._id == tabataId);
+            if(!tabata){
+               const error = new Error('Tabata doesnt exist');
+               error.statusCode = 500;
+               throw error;
+            }     
+            tabata.name = name;
+            tabata.details.work = work;
+            tabata.details.rest = rest;
+            tabata.details.rounds = rounds;
+            tabata.details.prepare = prepare;
+            tabata.description = description;
+            return user.save();
+        })
+        .then(result => {
+            res.status(200).json({message: 'tabata updated successfully'});
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 
 };
--- routes/actions.js
+++ routes/actions.js
@@ -2,19 +2,37 @@ const express = require('express');
 
 const actionsController = require('../controllers/actions');
 
+const { body } = require('express-validator/check');
+
 const router = express.Router();
 
 const isAuth = require('../middleware/is-auth');
 
-
-
 router.get('/tabatas', isAuth, actionsController.getTabatas);
 
-router.post('/tabata', isAuth, actionsController.postTabata);
+router.post('/tabata', [
+    body('name')
+        .isLength({ min: 1 })
+        .withMessage('Name must be at least of length 1'),
+    body('work')
+        .isNumeric()
+        .withMessage('Work must be a number'),
+    body('rest')
+        .isNumeric()
+        .withMessage('Work must be a number'),
+    body('rounds')
+        .isNumeric()
+        .withMessage('Work must be a number'),
+    body('prepare')
+        .isNumeric()
+        .withMessage('Work must be a number')
+
+], isAuth, actionsController.postTabata);
 
 router.get('/tabata/:tabataId', isAuth, actionsController.getTabata);
 
+router.delete('/tabata/:tabataId', isAuth, actionsController.deleteTabata);
 
-
+router.put('/tabata/:tabataId', isAuth, actionsController.updateTabata);
 
 module.exports = router;
