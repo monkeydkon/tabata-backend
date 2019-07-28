43fd9e245b35c43531512c041b9e0a3a4d7fee50
MonkeyDkon <kon_fu2@hotmail.com>
2019-07-28 20:34:06 +0300

    ready

--- passport/passport-setup.js
+++ passport/passport-setup.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const keys = require('../variables/keys');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
