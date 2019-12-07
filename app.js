78b5cfcef196aba065af45fca737b2f1489059fd
monkeydkon <kon_fu2@hotmail.com>
2019-12-07 17:40:53 +0200

    tryin

--- app.js
+++ app.js
@@ -10,16 +10,18 @@ const serviceAccount = require("./firebase-config.js");
 const admin = require('firebase-admin');
 
 admin.initializeApp({
-    type: process.env.FIREBASE_CONFIG_TYPE,
-    project_id: process.env.FIREBASE_CONFIG_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_CONFIG_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_CONFIG_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CONFIG_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CONFIG_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_CONFIG_AUTH_URI,
-    token_uri: process.env.FIREBASE_CONFIG_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_CONFIG_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CONFIG_CLIENT_X509_CERT_URL,
+    credential: admin.credential.cert({
+        type: process.env.FIREBASE_CONFIG_TYPE,
+        project_id: process.env.FIREBASE_CONFIG_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_CONFIG_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_CONFIG_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.FIREBASE_CONFIG_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CONFIG_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_CONFIG_AUTH_URI,
+        token_uri: process.env.FIREBASE_CONFIG_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_CONFIG_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CONFIG_CLIENT_X509_CERT_URL
+    })
 });
 
 const app = express();
