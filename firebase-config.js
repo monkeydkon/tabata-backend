c5d104808351363e7a6a6131a63ffa819de67c91
MonkeyDkon <monkeydkon@pop-os.localdomain>
2019-12-07 17:12:04 +0200

    firebase creds tryin

--- firebase-config.js
+++ firebase-config.js
@@ -8,5 +8,5 @@ module.exports = {
   auth_uri: process.env.FIREBASE_CONFIG_AUTH_URI,
   token_uri: process.env.FIREBASE_CONFIG_TOKEN_URI,
   auth_provider_x509_cert_url: process.env.FIREBASE_CONFIG_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CONFIG_CLEINT_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CONFIG_CLIENT_X509_CERT_URL,
 }
